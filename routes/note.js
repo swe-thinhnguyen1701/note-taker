5c01b5e390c94aae89c66ace4b5140594a7dfa03
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-06-13 17:20:08 -0500

    modify note route to run more effectively

--- routes/note.js
+++ routes/note.js
@@ -1,18 +1,21 @@
 const express = require("express");
-const app = express();
+const Crud = require("../helpers/Crud");
 const fs = require("fs");
 const route = express.Router();
-const dataPathName = "./db/db.json";
+
+const crud = new Crud();
+const DATA_BASE = crud.getDataBase();
 
 route.get("/", (req, res) => {
-    fs.readFile(dataPathName, "utf8", (err, data) => {
-        if(err){
-            console.log("Fail in reading file");
-            res.status(500).send("Interal issue");
-        }else{
-            res.json(JSON.parse(data));
-        }
-    });
+    // fs.readFile(dataPathName, "utf8", (err, data) => {
+    //     if(err){
+    //         console.log("Fail in reading file");
+    //         res.status(500).send("Interal issue");
+    //     }else{
+    //         res.json(JSON.parse(data));
+    //     }
+    // });
+    res.json(DATA_BASE);
 });
 
 route.post("/", (req, res) => {
