e18c9eec1edba0c6320ed55de3d5c5e25d362c8c
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-06-13 17:41:40 -0500

    modify route to return data

--- routes/note.js
+++ routes/note.js
@@ -1,21 +1,12 @@
 const express = require("express");
-const Crud = require("../helpers/Crud");
-const fs = require("fs");
 const route = express.Router();
-
+const Crud = require("../helpers/Crud");
 const crud = new Crud();
-const DATA_BASE = crud.getDataBase();
+
 
 route.get("/", (req, res) => {
-    // fs.readFile(dataPathName, "utf8", (err, data) => {
-    //     if(err){
-    //         console.log("Fail in reading file");
-    //         res.status(500).send("Interal issue");
-    //     }else{
-    //         res.json(JSON.parse(data));
-    //     }
-    // });
-    res.json(DATA_BASE);
+    const DATA_BASE = crud.getDataBase();
+    res.json(JSON.parse(DATA_BASE));
 });
 
 route.post("/", (req, res) => {
