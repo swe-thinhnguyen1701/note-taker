f3d564470e2077cc36f13812b0741b893dab5b84
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-06-13 19:04:56 -0500

    fix DESTINATION data field

--- helpers/FileWriter.js
+++ helpers/FileWriter.js
@@ -1,16 +1,18 @@
 const fs = require("fs");
 
-const DESTINATION = "../db/db.json";
+const DESTINATION = "./db/db.json";
 
 class FileWriter {
     constructor(data) {
         this.data = data;
+        // console.log(this.data);
     }
 
     saveData() {
         fs.writeFile(DESTINATION, this.data, error => {
-            error ? console.log("ERROR while writing") : console.log(`new data ${this.data.title} is saved!`);
-        })
+            error ? console.log("ERROR while writing") : console.log(`new data is saved!`);
+        });
+
     }
 }
 
