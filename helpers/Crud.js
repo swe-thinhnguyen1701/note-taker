089ffaa5dffae9e7946157c5b16c6007e80941f2
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-06-15 17:32:24 -0500

    add doc comment

--- helpers/Crud.js
+++ helpers/Crud.js
@@ -1,25 +1,41 @@
-const data= require("../db/db.json");
+const data = require("../db/db.json");
 const FileReader = require("./FileReader");
 const FileWriter = require("./FileWriter");
 const idGenerator = require("./idGenerator");
 
 const fileReader = new FileReader();
 
+/**
+ * Class representing CRUD operations.
+ */
 class Crud {
-    constructor() { 
+    constructor() {
+        /** 
+         * @property {Array} DATA_BASE - Array to hold database items.
+         */
         this.DATA_BASE = [];
         this.loadData();
     }
 
-    async loadData () {
-        try{
+    /**
+    * Asynchronously loads data from a file and initializes the DATA_BASE property.
+    * @async
+    */
+    async loadData() {
+        try {
             const data = await fileReader.getData();
             this.DATA_BASE = JSON.parse(data) || [];
-        }catch (error){
+        } catch (error) {
             this.DATA_BASE = [];
         }
     }
 
+    /**
+     * Inserts a new item into the database.
+     * @param {Object} data - The data object to insert.
+     * @param {string} data.title - The title of the data item.
+     * @returns {string} Confirmation message indicating the item is added.
+     */
     insert(data) {
         this.data = data;
         const id = idGenerator();
@@ -31,7 +47,12 @@ class Crud {
         return `${this.data.title} is ADDED`;
     }
 
-    delete (id){
+    /**
+     * Deletes an item from the database by its ID.
+     * @param {string} id - The ID of the item to delete.
+     * @returns {string} Confirmation message indicating the item is deleted.
+     */
+    delete(id) {
         const updateDB = this.DATA_BASE.filter(data => data.id !== id);
         this.DATA_BASE = updateDB;
 
@@ -40,16 +61,21 @@ class Crud {
         return `Item with ID(${id}) is DELETED`;
     }
 
+
+    /**
+     * Retrieves the current state of the database.
+     * @returns {Array} The current database array.
+     */
     getDataBase() {
         this.loadData();
         return this.DATA_BASE;
     }
 
-    generateNewId(){
-        return `${this.DATA_BASE.length}`;
-    }
-
-    async writeDataBase(){
+    /**
+     * Asynchronously writes the current state of the database to a file.
+     * @async
+     */
+    async writeDataBase() {
         const fileWriter = new FileWriter(JSON.stringify(this.DATA_BASE));
         await fileWriter.saveData();
     }
